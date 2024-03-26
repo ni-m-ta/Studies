9a645ddbf146626fd02aea1e40045aff877984c6
Tatsuya Nisato <tatsuya.nisato@colorkrew.com>
2024-03-26 22:01:36 +0900

    20240326

--- Tests/test.py
+++ Tests/test.py
@@ -1,15 +1,3 @@
-N = int(input())
-coordinates = []
-for i in range(N):
-    x,y = input().split()
-    coordinates.append((x,y))
-
-for i in range(N-1):
-    for j in range(i+1,N):
-        x1 = coordinates[i][0]
-        y1 = coordinates[i][1]
-        x2 = coordinates[j][0]
-        y2 = coordinates[j][1]
-        dx = x1-x2
-        dy = y1-y2
-        
+a = "1.5"
+b = float(a)
+print(type(b))
